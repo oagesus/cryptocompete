5d8bb07d31a7d226faf6fc5c2711cf385d688cf6
oagesus <firework.channel@gmail.com>
2026-01-03 14:52:57 +0100

    navbar style chagnes

--- cryptocompete-web/components/navbar.tsx
+++ cryptocompete-web/components/navbar.tsx
@@ -8,8 +8,8 @@ export async function Navbar() {
   const user = await getUser();
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background px-6">
-      <div className="mx-auto flex h-14 w-full max-w-screen-xl items-center justify-between">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/80 backdrop-blur-sm px-6">
+      <div className="mx-auto flex h-16 w-full max-w-screen-xl items-center justify-between">
         <Link href="/" className="text-2xl font-thin">
           CryptoCompete
         </Link>
